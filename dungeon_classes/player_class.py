77d4ee6722f964458671106903e48156965bea2f
ryan <ryan.h153@gmail.com>
2020-10-25 18:01:33 -0600

    Updating requirements

--- dungeon_classes/player_class.py
+++ dungeon_classes/player_class.py
@@ -9,7 +9,7 @@ class Player:
 		self.audumbla_wins = 0
 		self.math_game_wins = 0
 		self.loki_wins = 0
-		self.letter_tiles = ['Q', 'U', 'O', 'T', 'H']
+		self.letter_tiles = []
 
 	def move(self, direction):
 		if direction == "n":
